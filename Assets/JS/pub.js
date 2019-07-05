d894424ab29cbf6f030fd4ebe21317878a8a5035
d-veazie <dmanvz@gmail.com>
2019-07-05 17:20:17 -0600

    updated review header and enter/submit function

--- Assets/JS/pub.js
+++ Assets/JS/pub.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
     $("#map").hide();
-     
+    
+    let input = document.getElementById("search");
+        // Execute a function when the user releases a key on the keyboard
+        input.addEventListener("keyup", function(event) {
+            // Number 13 is the "Enter" key on the keyboard
+            if (event.keyCode === 13) {
+                // Cancel the default action, if needed
+                event.preventDefault();
+                // Trigger the button element with a click
+                document.getElementById("submit").click();
+            }
+        });
 
     function displayPub() {
 
